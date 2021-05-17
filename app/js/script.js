c48d9d6516afeac7d9b94cd7dd6e505af1265aa8
big-viktor <79458452+big-viktor@users.noreply.github.com>
2021-05-17 22:54:28 +0300

    ready slider

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+
 const burger = document.getElementById("burger"); 
 const menuContent = document.querySelector('.menu_content')
 
@@ -25,85 +26,70 @@ const sliderBox = [];
       let sliders = slider.children[i];
         sliderBox.push(sliders);
     }
+    console.log(sliderBox)
 
-console.log(sliderBox)
+  
+    let index = 0;
+    console.log(index)
 
-let index = 0;
-console.log(index)
 
+   sliderBox[index];
 
- let arr = sliderBox[index];
-console.log(arr)
 
 nextSlide = () => {
-    if(index === (sliderBox.length -2)){
-        next.classList.add('delet_div');
-    }
-    else if(index === -1 ){
-        alert('-12')
-        index = 0;
-        index++;
-    }
-    arr = sliderBox[index];
-    arr.classList.add('delet_div');
+    
+    if(index === 0){
+        next.classList.remove('active');
+        previous.classList.remove('delet_div');
+        index -1;
+        sliderBox[index++].classList.add('delet_div');
+        // alert('meseg:-1');
+        console.log(sliderBox);
+        console.log(index);
+    }else{
+        // alert('sad')
+        sliderBox[index++].classList.add('delet_div');
+        console.log(sliderBox);
+        console.log(index);
+        if(index === (sliderBox.length -1)){
+            next.classList.add('delet_div');
+            sliderBox[index--];
+            // alert("Last Slide");
+            console.log(sliderBox);
+            console.log(index);
+        }
 
-    index++;
-    
-    console.log(index)
-    console.log(arr)
-    next.classList.remove('active');
-    previous.classList.remove('delet_div');
-    
+    }
    
-
 };
 
- backSlide = () => {
+backSlide = () => {
     
-    arr = sliderBox[index];
-    arr.classList.remove('delet_div');
-    console.log(arr)
-    index--;
-    console.log(index)
-
-    next.classList.remove('delet_div');
-    if(index === -1){
-        console.log(index)
-        alert('stop!!')
-        index = -1;
-        next.classList.add('active');
-        previous.classList.add('delet_div');
-
-        }else if(index === 0 ){
-            alert('stop')
-            index = -1;
+    if(index === 0){
+        // alert('dobavim');
+        sliderBox[index--].classList.remove('delet_div');
+        console.log(index);
+        console.log(sliderBox);
+        if(index === -1){
+            // alert("delet-btn-back")
+            previous.classList.add('delet_div');
+            next.classList.add('active');
             index++;
         }
+    }else{
+        sliderBox[index--].classList.remove('delet_div');
+        // alert("dobavim +1")
+        next.classList.remove('delet_div');
+        console.log(sliderBox);
+        console.log(index);
+        if(index === -1){
+            // alert("delet-btn-back")
+            previous.classList.add('delet_div');
+            index++;
+        }
+    }
 };
 
 next.addEventListener('click', nextSlide);
 previous.addEventListener(('click'), backSlide);
 
-// 
-    // index--;
-    // console.log(index)
-    // console.log(sliderBox[index])
-    // console.log(sliderBox)
-    //
-
-
- // index++
-    // console.log(index)
-
-    // sliderBox[index].classList.add('delet_div');
-
-    
-    
-    // console.log(sliderBox[index])
-    
-    
-
-// const sliderDisplay = () => {
-   
-//     sliderDisplay();
-// }
